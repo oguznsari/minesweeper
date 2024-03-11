4e6519efbe29a00ef5707a5ad2c1e80ce0479c48
Oğuzhan SARI <oguzn.sari@gmail.com>
2024-03-11 23:54:30 +0300

    MineSweeper header

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import { GameSeedInput } from "@/components/GameSeedInput";
 
 export default function Home() {
   return (
-    <div className="mt-20">
+    <div className="mt-20 flex flex-col items-center">
+      <h1 className="my-8 text-2xl">Minesweeper</h1>
       <GameSeedInput />
     </div>
   );
